f36c0b1150f0576c3d03f97ed7af82b632a59a8a
threexc <tvgamblin@gmail.com>
2021-04-24 20:22:08 -0400

    Optimization Fixes
    - Use pandas to load data
    - Simplify TowerList a bit
    - Remove old swap file
    - Refactor for speed
    
    Signed-off-by: threexc <tvgamblin@gmail.com>

--- walksignal/data.py
+++ walksignal/data.py
@@ -2,6 +2,7 @@
 import csv
 import sys
 import numpy as np
+import pandas as pd
 import time
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
@@ -15,71 +16,83 @@ class DataSet:
         self.dataset_name = self.data_path.rsplit('/', 1)[1]
         self.map_path = self.data_path + "/map.png"
         self.bbox_path = self.data_path + "/bbox.txt"
-        self.data_matrix = np.array(utils.read_csv(data))
-        self.reference_matrix = np.array(utils.read_csv(reference))
 
-        # Determine start and end times of test and get a time range for the trip
-        self.time_range = np.array(self.data_matrix[1:,7], dtype=float)
-        self.start_time = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(self.time_range[0]/1000.))
-        self.end_time = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(self.time_range[-1]/1000.))
+        self.start_time = time.time()
+        self.data_matrix = pd.read_csv(data)
+        self.duration = time.time() - self.start_time
+        print("Done loading data_matrix in {:.2f} seconds".format(self.duration))
+
+        self.start_time = time.time()
+        self.time_range = np.array(self.data_matrix['measured_at'], dtype=float)
+        self.data_start_time = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(self.time_range[0]/1000.))
+        self.data_end_time = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(self.time_range[-1]/1000.))
         self.normalized_time_range = (self.time_range - self.time_range[0])/1000
-        self.lat = np.array(self.data_matrix[1:,4], dtype=float)
-        self.lon = np.array(self.data_matrix[1:,5], dtype=float)
-
-        # get signal strength in dBm
-        self.signal_range = np.array(self.data_matrix[1:,6], dtype=float)
-
-        # get physical cell IDs
-        self.pcis = np.array(self.data_matrix[1:,15], dtype=float)
-
-        # get device speed
-        self.speed_values = np.array(self.data_matrix[1:,9], dtype=float)
-        self.mcc = np.array(self.data_matrix[1:,0], dtype=int)
-        self.mnc = np.array(self.data_matrix[1:,1], dtype=int)
-        self.lac = np.array(self.data_matrix[1:,2], dtype=int)
-        self.cellid = np.array(self.data_matrix[1:,3], dtype=int)
-        self.rating = np.array(self.data_matrix[1:,8], dtype=float)
-        self.direction = np.array(self.data_matrix[1:,10], dtype=float)
-        self.timing_advance = np.array(self.data_matrix[1:,12], dtype=float)
-
-        # get access types and convert them to usable format
-        self.access_type_range = np.array(self.data_matrix[1:,11])
-        self.access_type_color_codes = np.zeros(len(self.access_type_range), dtype="str")
-        for x in range(len(self.access_type_range)):
-            if self.access_type_range[x] == "LTE":
-                self.access_type_color_codes[x] = "r"
-            elif self.access_type_range[x] == "LTE+":
-                self.access_type_color_codes[x] = "b"
-            elif self.access_type_range[x] == "UMTS":
-                self.access_type_color_codes[x] = "g"
-            elif self.access_type_range[x] == "HSPA+":
-                self.access_type_color_codes[x] = "k"
-            else:
-                self.access_type_color_codes[x] = "y"
-
-        self.hash = {}
-        self.hash['time'] = self.normalized_time_range
-        self.hash['signal_strength'] = self.signal_range
-        self.hash['pcis'] = self.pcis
-        self.hash['speed'] = self.speed_values
-        self.hash['mcc'] = self.mcc
-        self.hash['mnc'] = self.mnc
-        self.hash['lac'] = self.lac
-        self.hash['cellid'] = self.cellid
-        self.hash['rating'] = self.rating
-        self.hash['direction'] = self.direction
-        self.hash['advance'] = self.timing_advance
-
-        self.tower_list = TowerList(self.data_matrix, self.reference_matrix)
-        self.tower_lat_data = self.tower_list.lats
-        self.tower_lon_data = self.tower_list.lons
-        self.lat_data = self.lat
-        self.lon_data = self.lon
-        self.signal_data = self.signal_range
+        self.lat = np.array(self.data_matrix['lat'], dtype=float)
+        self.lon = np.array(self.data_matrix['lon'], dtype=float)
+        self.signal = np.array(self.data_matrix['signal'], dtype=float)
+        self.pcis = np.array(self.data_matrix['pci'], dtype=float)
+        self.speed = np.array(self.data_matrix['speed'], dtype=float)
+        self.mcc = np.array(self.data_matrix['mcc'], dtype=int)
+        self.mnc = np.array(self.data_matrix['mnc'], dtype=int)
+        self.lac = np.array(self.data_matrix['lac'], dtype=int)
+        self.cellid = np.array(self.data_matrix['cellid'], dtype=int)
         self.mcc_u = np.unique(self.mcc)
         self.mnc_u = np.unique(self.mnc)
         self.lac_u = np.unique(self.lac)
         self.cellid_u = np.unique(self.cellid)
+        self.rating = np.array(self.data_matrix['rating'], dtype=float)
+        self.direction = np.array(self.data_matrix['direction'], dtype=float)
+        self.timing_advance = np.array(self.data_matrix['ta'], dtype=float)
+        self.access_type = self.data_matrix['act']
+        self.access_type_color_codes = np.zeros(len(self.access_type),dtype="str")
+        for x in range(len(self.access_type)):
+            if self.access_type[x] == "LTE":
+                self.access_type_color_codes[x] = "r"
+            elif self.access_type[x] == "LTE+":
+                self.access_type_color_codes[x] = "b"
+            elif self.access_type[x] == "UMTS":
+                self.access_type_color_codes[x] = "g"
+            elif self.access_type[x] == "HSPA+":
+                self.access_type_color_codes[x] = "k"
+            else:
+                self.access_type_color_codes[x] = "y"
+
+        self.duration = time.time() - self.start_time
+        print("Done loading dataset properties in {:.2f} seconds".format(self.duration))
+
+        self.start_time = time.time()
+        self.reference_matrix = pd.read_csv(reference)
+        self.reference_matrix = self.reference_matrix.loc[self.reference_matrix['cell'].isin(self.cellid_u)]
+        self.duration = time.time() - self.start_time
+        print("Done loading reference_matrix in {:.2f} seconds".format(self.duration))
+
+        self.start_time = time.time()
+        self.ref_mcc = np.array(self.reference_matrix['mcc'])
+        self.ref_mnc = np.array(self.reference_matrix['net'])
+        self.ref_lac = np.array(self.reference_matrix['area'])
+        self.ref_cellid = np.array(self.reference_matrix['cell'])
+        self.ref_lon = np.array(self.reference_matrix['lon'])
+        self.ref_lat = np.array(self.reference_matrix['lat'])
+        self.ref_access = np.array(self.reference_matrix['radio'])
+        self.duration = time.time() - self.start_time
+        print("Done loading reference properties in {:.2f} seconds".format(self.duration))
+
+        self.start_time = time.time()
+        self.tower_id_list = []
+        self.tower_list = []
+        self.lat_data = self.lat
+        self.lon_data = self.lon
+        self.get_tower_ids()
+        self.get_dataset_tower_data()
+        self.get_tower_data_points()
+        self.get_distances_to_towers()
+        self.get_power()
+        self.tower_lat_data = self.get_tower_lats()
+        self.tower_lon_data = self.get_tower_lons()
+        self.duration = time.time() - self.start_time
+        print("Done loading tower properties in {:.2f} seconds".format(self.duration))
+
+        self.start_time = time.time()
         self.plot_map = None
         self.map_bbox = None
         self.get_map_and_bbox()
@@ -92,6 +105,8 @@ class DataSet:
         self.avg_lon_diff = np.average(np.ediff1d(self.lon_data))
         self.distances = np.array([])
         self.plotrange = np.linspace(1, 500, 250)
+        self.duration = time.time() - self.start_time
+        print("Done loading plot properties in {:.2f} seconds".format(self.duration))
 
     def get_map_and_bbox(self):
         self.plot_map = plt.imread(self.map_path)
@@ -100,50 +115,37 @@ class DataSet:
     def set_image(self):
         self.ax1.imshow(self.plot_map, zorder=0, extent = self.map_bbox[0], aspect="equal")
 
-class TowerList:
-    def __init__(self, data, reference):
-        self.tower_id_list = []
-        self.tower_list = []
-        self.cellid_list = []
-        self.data = np.array(data)
-        self.lats = np.array([])
-        self.lons = np.array([])
-
-        self.reference_data = np.array(reference)
-
-        # Fill tower_id_list with tuples of mcc, mnc, lac, cellid
-        self.get_tower_ids()
-
-        # Add detected towers to the tower_list by comparing mcc, mnc,
-        # lac, cellid with the reference file
-        self.get_towers()
-
-        # Add all relevant datapoints to each tower's data_points list
-        self.get_tower_data_points()
-        self.get_distances()
-        self.get_power()
-        self.get_tower_lats()
-        self.get_tower_lons()
-
     def get_tower_ids(self):
-        for row in self.data:
-            if row[3] not in self.cellid_list:
-                self.cellid_list.append(row[3])
-                self.tower_id_list.append((row[0], row[1], row[2], row[3]))
+        for row in range(len(self.cellid_u)):
+            self.tower_id_list.append((self.mcc[row], self.mnc[row], self.lac[row], self.cellid_u[row]))
 
-    def get_towers(self):
-        for row in self.reference_data:
-            for entry in self.tower_id_list:
-                if ((row[1] == entry[0]) and (row[2] == entry[1]) and (row[3] == entry[2]) and (row[4] == entry[3])):
-                    self.tower_list.append(Tower(row[0], row[1], row[2], row[3], row[4], row[6], row[7], row[8], row[9]))
+    def get_dataset_tower_data(self):
+        print(self.reference_matrix['cell'])
+        for tower_id in self.tower_id_list:
+            print(type(tower_id[3]))
+            if tower_id[3] in self.reference_matrix['cell'].unique():
+                print("Found {0} in reference".format(tower_id[3]))
+                for index, row in self.reference_matrix.iterrows():
+                    print(type(row['cell']), type(tower_id[3]))
+                    if row['cell'] == tower_id[3]:
+                        print("Added {0}".format(row['cell']))
+                        self.tower_list.append(Tower(row['radio'], row['mcc'], row['net'], row['area'], row['cell'], row['lon'], row['lat'], row['range'], row['samples']))
+            else:
+                self.tower_list.append(Tower(mcc=tower_id[0], mnc=tower_id[1], lac=tower_id[2], cellid=tower_id[3]))
 
     def get_tower_data_points(self):
-        for tower in self.tower_list:
-            for row in self.data:
-                if ((tower.mcc == row[0]) and (tower.mnc == row[1]) and (tower.lac == row[2]) and (tower.cellid == row[3])):
-                    tower.data_points.append(TowerDataPoint(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[14], row[15]))
+        for index, row in self.data_matrix.iterrows():
+            for tower in self.tower_list:
+                if (tower.cellid == row['cellid']):
+                    tower.data_points.append(TowerDataPoint(row['mcc'],
+                        row['mnc'], row['lac'], row['cellid'],
+                        row['lat'], row['lon'], row['signal'],
+                        row['measured_at'], row['rating'],
+                        row['speed'], row['direction'],
+                        row['act'], row['ta'],
+                        row['pci']))
 
-    def get_distances(self):
+    def get_distances_to_towers(self):
         for tower in self.tower_list:
             tower.get_distances()
 
@@ -153,20 +155,22 @@ class TowerList:
                 tower.signal_power.append(float(datapoint.signal))
 
     def get_tower_lats(self):
-        for tower in self.tower_list:
-            self.lats = np.concatenate([self.lats, [float(tower.lat)]])
+        tower_lats = [tower.lat for tower in self.tower_list]
+        return tower_lats
 
     def get_tower_lons(self):
-        for tower in self.tower_list:
-            self.lons = np.concatenate([self.lons, [float(tower.lon)]])
+        tower_lons = [tower.lon for tower in self.tower_list]
+        return tower_lons
 
-    def get_tower_data(self, mcc, mnc, lac, cellid):
+    def get_tower_stats(self, mcc, mnc, lac, cellid):
         for tower in self.tower_list:
-            if ((tower.mcc == mcc) and (tower.mnc == mnc) and (tower.lac == lac) and (tower.cellid == cellid)):
+            if ((str(tower.mcc) == mcc) and (str(tower.mnc) == mnc) and (str(tower.lac) == lac) and (str(tower.cellid) == cellid)):
+                print(tower.cellid)
+                print(tower.lat, tower.lon)
                 return tower
 
 class Tower:
-    def __init__(self, signal_type, mcc, mnc, lac, cellid, lon, lat, tower_range, samples):
+    def __init__(self, signal_type=None, mcc=None, mnc=None, lac=None, cellid=None, lon=None, lat=None, tower_range=None, samples=None):
         self.mcc = mcc
         self.mnc = mnc
         self.lac = lac
@@ -182,11 +186,14 @@ class Tower:
 
     def get_distances(self):
         self.distances.clear()
-        for datapoint in self.data_points:
-            self.distances.append(utils.get_distance(self.lat, self.lon, datapoint.lat, datapoint.lon) * 1000)
+        if (self.lat is None) or (self.lon is None):
+            print("Can't get distances for {0} {1} {2} {3}, lat/lon not available".format(self.mcc, self.mnc, self.lac, self.cellid))
+        else:
+            for datapoint in self.data_points:
+                self.distances.append(utils.get_distance(self.lat, self.lon, datapoint.lat, datapoint.lon) * 1000)
 
 class TowerDataPoint:
-    def __init__(self, mcc, mnc, lac, cellid, lat, lon, signal, measured_at, rating, speed, direction, access_type, timing_advance, tac, pci):
+    def __init__(self, mcc, mnc, lac, cellid, lat, lon, signal, measured_at, rating, speed, direction, access_type, timing_advance, pci):
       self.mcc = mcc
       self.mnc = mnc
       self.lac = lac
@@ -200,5 +207,4 @@ class TowerDataPoint:
       self.direction = direction
       self.access_type = access_type
       self.timing_advance = timing_advance
-      self.tac = tac
       self.pci = pci
