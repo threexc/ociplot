860dc33002e863264ce66c2d3d8542f7e604f202
threexc <tvgamblin@gmail.com>
2021-05-02 20:29:43 -0400

    Add general ABG and CI models; shorten variable names
    Signed-off-by: threexc <tvgamblin@gmail.com>

--- walksignal/equations.py
+++ walksignal/equations.py
@@ -29,27 +29,38 @@ def gplt_rwm_fpd3d(obs_dens, absorption, x_range):
 
     return rwm_y
 
-def pl_fs(distance, frequency):
-    return 20 * np.log10(4 * constants.pi * distance * frequency / constants.speed_of_light)
+def pl_fs(dist, freq):
+    return 20 * np.log10(4 * constants.pi * dist * freq / constants.speed_of_light)
 
-def pl_los_5gcm_ci(distance, frequency):
-    return 32.4 + 20 * np.log10(distance) + 20 * np.log10(frequency)
+def pl_los_5gcm_ci(dist, freq):
+    return 32.4 + 20 * np.log10(dist) + 20 * np.log10(freq)
 
-def pl_nlos_5gcm_ci(distance, frequency):
-    return 32.4 + 30 * np.log10(distance) + 20 * np.log10(frequency)
+def pl_nlos_5gcm_ci(dist, freq):
+    return 32.4 + 30 * np.log10(dist) + 20 * np.log10(freq)
 
-def pl_nlos_5gcm_abg(distance, frequency):
-    return 19.2 + 34 * np.log10(distance) + 23 * np.log10(frequency)
+def pl_nlos_5gcm_abg(dist, freq):
+    return 19.2 + 34 * np.log10(dist) + 23 * np.log10(freq)
 
-def v_pl_fs(array, frequency):
-    return np.array([pl_fs(xi, frequency) for xi in array])
+def v_pl_fs(array, freq):
+    return np.array([pl_fs(xi, freq) for xi in array])
 
-def v_pl_los_5gcm_ci(array, frequency):
-    return np.array([pl_los_5gcm_ci(xi, frequency) for xi in array])
+def v_pl_los_5gcm_ci(array, freq):
+    return np.array([pl_los_5gcm_ci(xi, freq) for xi in array])
 
-def v_pl_nlos_5gcm_ci(array, frequency):
-    return np.array([pl_nlos_5gcm_ci(xi, frequency) for xi in array])
+def v_pl_nlos_5gcm_ci(array, freq):
+    return np.array([pl_nlos_5gcm_ci(xi, freq) for xi in array])
 
-def v_pl_nlos_5gcm_abg(distance, frequency):
-    return np.array([pl_nlos_5gcm_abg(xi, frequency) for xi in array])
+def v_pl_nlos_5gcm_abg(dist, freq):
+    return np.array([pl_nlos_5gcm_abg(xi, freq) for xi in array])
 
+def pl_abg(dist, freq, alpha, beta, gamma, sigma, ref_dist=1, ref_freq=1000000000):
+    return 10 * alpha * np.log10(dist/ref) + beta + 10 * gamma * np.log10(freq/ref_freq) + np.random.normal(0, sigma)
+
+def v_pl_abg(array, freq, alpha, beta, gamma, sigma, ref_dist=1, ref_freq=1000000000):
+    return np.array([pl_abg(xi, freq, alpha, beta, gamma, sigma, ref_dist, ref_freq) for xi in array)
+    
+def pl_ci(dist, freq, sigma, pl_exp, ref_dist=1):
+    return pl_fs(ref_dist, freq) + 10 * pl_exp * np.log10(dist/ref_dist) + np.random.normal(0, sigma)
+
+def v_pl_ci(array, freq, sigma, pl_exp, ref_dist=1):
+    return np.array([pl_ci(xi, freq, sigma, pl_exp, ref_dist) for xi in array)
