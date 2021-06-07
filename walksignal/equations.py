98416cbf3fd62a2febf210dba4da4613f2df602f
Trevor Gamblin <tvgamblin@gmail.com>
2021-06-06 21:42:49 -0400

    Add plots for OH models; fix equations
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- walksignal/equations.py
+++ walksignal/equations.py
@@ -75,12 +75,12 @@ def pl_oh_corr_factor(ue_height, freq, large_city=True):
         return (1.1 * np.log10(freq) - 0.7) * ue_height - (1.56 * np.log10(freq) - 0.8)
 
 def pl_oh_urban(dist, freq, bs_height, ue_height, large_city=True):
-    return 69.55 + 26.26 * np.log10(freq) + (44.9 - 6.55 * np.log10(bs_height)) * np.log10(dist) - 13.85 * np.log10(base_height) - pl_oh_corr_factor(ue_height, freq, large_city)
+    return 69.55 + 26.26 * np.log10(freq) + (44.9 - 6.55 * np.log10(bs_height)) * np.log10(dist) - 13.85 * np.log10(bs_height) - pl_oh_corr_factor(ue_height, freq, large_city)
 
 def pl_oh_suburban(dist, freq, bs_height, ue_height, large_city=False):
     return pl_oh_urban(dist, freq, bs_height, ue_height, large_city) - 2 * np.square(np.log10(freq / 28)) - 5.4
 
-def pl_oh_rural(dist, freq, bs_heigh, ue_height, large_city=False):
+def pl_oh_rural(dist, freq, bs_height, ue_height, large_city=False):
     return pl_oh_urban(dist, freq, bs_height, ue_height, large_city) - 4.78 * np.square(np.log10(freq)) + 18.33 * np.log10(freq) - 40.94
 
 def v_pl_oh_urban(array, freq, bs_height, ue_height, large_city=True):
