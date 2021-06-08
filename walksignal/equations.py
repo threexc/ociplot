Fix OH model distance calculation; fix plot scale
Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

walksignal/equations.py
@@ -67,7 +67,7 @@ def v_pl_fs(array, freq):
 
 def pl_oh_corr_factor(ue_height, freq, large_city=True):
     if (large_city == True):
-        if (freq < 300000000):
+        if (freq < 300):
             return 8.29 * np.square(np.log10(1.54 * ue_height)) - 1.1
         else:
             return 3.2 * np.square(np.log10(11.75 * ue_height)) - 4.97
@@ -75,7 +75,7 @@ def pl_oh_corr_factor(ue_height, freq, large_city=True):
         return (1.1 * np.log10(freq) - 0.7) * ue_height - (1.56 * np.log10(freq) - 0.8)
 
 def pl_oh_urban(dist, freq, bs_height, ue_height, large_city=True):
-    return 69.55 + 26.26 * np.log10(freq) + (44.9 - 6.55 * np.log10(bs_height)) * np.log10(dist) - 13.85 * np.log10(bs_height) - pl_oh_corr_factor(ue_height, freq, large_city)
+    return 69.55 + 26.26 * np.log10(freq) + (44.9 - 6.55 * np.log10(bs_height)) * np.log10(dist / 1000) - 13.85 * np.log10(bs_height) - pl_oh_corr_factor(ue_height, freq, large_city)
 
 def pl_oh_suburban(dist, freq, bs_height, ue_height, large_city=False):
     return pl_oh_urban(dist, freq, bs_height, ue_height, large_city) - 2 * np.square(np.log10(freq / 28)) - 5.4