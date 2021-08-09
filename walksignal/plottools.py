3fe111235ba9fdff32c4fa327f4f03e77208bd43
Trevor Gamblin <tvgamblin@gmail.com>
2021-08-09 16:52:55 -0400

    Start adding abstracted plot lines
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- walksignal/plottools.py
+++ walksignal/plottools.py
@@ -6,7 +6,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from typing import List
 from dataclasses import dataclass
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -26,4 +26,13 @@ class FreeSpaceLine:
     def get_pl(self, freq, tx_gain, rx_gain):
         return eq.v_pl_fs(self.power, freq, tx_gain, rx_gain)
 
+@dataclass
+class ABGLine(FreeSpaceLine):
+    alpha: float
+    beta: float
+    gamma: float
+    sigma: float
 
+    def get_pl(self, freq, tx_gain, rx_gain):
+        return eq.v_pl_abg(self.power, freq, alpha, beta, gamma, sigma,
+                ref_dist, ref_freq, tx_gain, rx_gain)
