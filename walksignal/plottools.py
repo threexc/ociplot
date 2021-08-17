95711d60aff321588bbd4092e92da65cf30f53aa
Trevor Gamblin <tvgamblin@gmail.com>
2021-08-17 18:08:26 -0400

    Remove duplicate model prototypes
    Complete model classes have been implemented in models.py, so these are
    no-longer needed.
    
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- walksignal/plottools.py
+++ walksignal/plottools.py
@@ -14,35 +14,3 @@ class MplCanvas(FigureCanvas):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = self.fig.add_subplot(111)
         super(MplCanvas, self).__init__(self.fig)
-
-@dataclass
-class FreeSpaceLine:
-    """Class containing information about a free space path loss model.
-    It is also a parent class for other path loss models that use the
-    free space model as a baseline."""
-    name: str
-    domain: List[float]
-    xlabel: str
-    ylabel: str
-    colormap: str
-
-    def get_pl(self, freq, tx_gain, rx_gain):
-        return md.v_pl_fs(self.domain, freq) - tx_gain - rx_gain
-
-@dataclass
-class ABGLine(FreeSpaceLine):
-    alpha: float
-    beta: float
-    gamma: float
-    sigma: float
-
-    def get_pl(self, freq, ref_dist, ref_freq, tx_gain, rx_gain):
-        return md.v_pl_abg(self.domain, freq, alpha, beta, gamma, sigma, ref_dist, ref_freq) - tx_gain - rx_gain
-
-@dataclass
-class CILine(FreeSpaceLine):
-    sigma: float
-    exp: float
-
-    def get_pl(self, freq, ref_freq, tx_gain, rx_gain):
-        return md.v_pl_ci(self.domain, freq, sigma, exp, ref_freq) - tx_gain - rx_gain 
